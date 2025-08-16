08b5d0986cd57d296f3d56489ffacebb7b3505a8
Deep-sarkar02 <srcs.pro5759@gmail.com>
2025-08-16 10:40:34 +0530

    Initial commit - portfolio files

--- src/sections/Exp.jsx
+++ src/sections/Exp.jsx
@@ -1,28 +1,69 @@
-import React from "react";
-import { div } from "three/tsl";
-const Exp = ()=>{
-    return(
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { workExperiences } from "../constants";
+import { OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import Developer from "../components/Developer";
+
+const Exp = () => {
+    const[animationName, setAnimationName] = React.useState('idle');
+    return (
         <section className="c-space my-20">
-            <div className="w-full text-white-600">
-                <h3 className="head-text"> My work Experience:</h3>
+             <div className="w-full text-white-600">
+                <h3 className="head-text">
+                    My Work
+                </h3>
 
                 <div className="work-container">
                     <div className="work-canvas">
                         <Canvas>
+                            <ambientLight intensity={7} />
+                            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                            <directionalLight position={[10, 10, 10]} intensity={1} />
+                            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} />
+                              
 
+                            <Suspense fallback={<CanvasLoader/>}>
+                                {/* Developer model component */}
+                                <Developer position-y={-3} scale={3} animationName={animationName} />
+                            </Suspense>
                         </Canvas>
                     </div>
 
+                    {/** now we are implement the right side of the experience */}
                     <div className="work-content">
-                        {/**splacin */}
-                        <div className="sm:py-10 py-5 sm:px-5 px-2.5" >
-                            
+                        <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+                            {workExperiences.map(({id, name, pos, icon, duration, title, animation}, index) => (
+                                <div key={id} className="work-content_container group" onClick={()=>setAnimationName(animation.toLowerCase())}  onPointerOver = {()=>setAnimationName(animation.toLowerCase())} onPointerOut = {()=>setAnimationName('idle')}>
+                                    <div className="flex flex-col h-full justify-center items-center py-2">
+                                        <div className="work-content_logo">
+                                            <img src={icon} alt="logo" className="w-full h-full" />
+                                        </div>
+
+                                        <div className="work-content_bar"/>
+                                    </div>
+
+
+                                    <div className="sm:p-5 px-2.5 py-5">
+                                        <p className="font-bold text-white-800">
+                                            {name}
+                                        </p>
+                                        <p className="text-sm mb-5">
+                                            {pos} -- {duration}
+                                        </p>
+                                        <p className="group-hover:text-white transition ease-in-out duration-500">
+                                            {title}
+                                            {/* Model from Ready Player Me with Adobe Mixamo animations */}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                </div>
-            </div>
-
+                </div> 
+             </div>
         </section>
     )
 }
+
 export default Exp;
